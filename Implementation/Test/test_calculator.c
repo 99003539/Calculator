bad21d21c88c13bf22881d7c11b61c29c72602d0
99003539 <78578046+99003539@users.noreply.github.com>
2021-02-26 16:12:52 +0530

    Update test_calculator.c

--- Implementation/Test/test_calculator.c
+++ Implementation/Test/test_calculator.c
@@ -21,10 +21,10 @@ void setUp(){}
 
 void tearDown(){}
 void test_interest_1(void){
-    TEST_ASSERT_EQUAL(2250, interest(10000, 3, 7.5));
+    TEST_ASSERT_EQUAL(2250, interest_test(10000, 3, 7.5));
 }
 void test_interest_2(void){
-    TEST_ASSERT_EQUAL(780, interest(5000, 2, 7.8));
+    TEST_ASSERT_EQUAL(780, interest_test(5000, 2, 7.8));
 }
 
 void test_leapyear_1(void){
