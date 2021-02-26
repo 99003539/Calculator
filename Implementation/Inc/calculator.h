2f80c6ed1db19babf0d3fa86e274d4aa6f51261e
stepin104510 <52899930+stepin104510@users.noreply.github.com>
2021-02-26 08:54:23 +0530

    inc updated

--- Implementation/Inc/calculator.h
+++ Implementation/Inc/calculator.h
@@ -1,4 +1,9 @@
 #ifndef _CALCULATOR_H_
 #define _CALCULATOR_H_
+#include<stdio.h>
+
+int interest();
+int interest1(int x, float y, float z);
+int interest_test(int x, float y, float z);
 int leap_year();
 #endif
